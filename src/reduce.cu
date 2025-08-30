72dc21ebe6f9799976f705f52d63476cf6304340
ricvigi <ricvigi@proton.me>
2025-08-31 00:26:31 +0200

    aggiunto qualche commentino

--- src/reduce.cu
+++ src/reduce.cu
@@ -19,11 +19,12 @@
 
 __device__ __forceinline__ float warp_reduce(float val)
 {
-	FULL_MASK = 0xffffffff;
+	FULL_MASK = 0xffffffff; //FULL_MASK è una variabile che dice a cuda quanti thread di questo warp devono partecipare nella collettiva. In parole povere è una bitmap. 0xffffffff diventa 1111 1111 1111 1111 1111 1111 1111 1111, che viene intepretato da cuda come "se il bit i è uguale a 1 allora il thread i parteciperà a questa operazione"
 # pragma unroll
 	for (unsigned int i = 16; i > 0; i /= 2)
 	{
-		val = max(val, __shfl_down_sync(FULL_MASK, val, i));
+    // __shfl_down_sync permette ai thread all'interno di un warp di mandarsi un valore, è come se fosse una send di MPI, con la differenza che si mandano questo valore senza passare dalla shared memory, ma utilizzando direttamente i registri! questo permette di eseguire una riduzione in maniera molto molto molto veloce!
+		val = max(val, __shfl_down_sync(FULL_MASK, val, i)); //__shfl_down_sync() manda al thread ad offset i il valore val. Il loop è necessario perché per effettuare una reduce completa con questa logica ogni thread deve ricevere e mandare esattamente log_2(n) elementi. Visto che un warp contiene 32 thread, questo loop può essere unrollato con la pragma perché il numero delle operazioni è costante.
 	}
 	return val;
 }
@@ -35,8 +36,9 @@ __global__ void reduce(float* inputs, unsigned int input_size, float* outputs)
 	 * Praticamente questa implementazione sfrutta delle operazioni che vengono eseguite a livello dei warp. Se vi ricordate, in cuda
 	 * i warp sono il più basso livello logico in cui le istruzioni vengono eseguite.
 	 * ATTENZIONE: per fare si che questo algoritmo funzioni, input_size DEVE essere una potenza di 2, quindi dovete paddare il vostro array finché non ha
-	 * la grandezza desiderata. Questo non influisce sulla correttezza del vostro algoritmo, vi dovete solo ricordare di paddare con un valore neutro per
-	 * la vostra operazione (nel caso del MAX il valore è -FLT_MAX oppure semplicemente FLT_MIN)
+	 * la grandezza desiderata (se l'array è lungo n, paddatelo alla potenza di 2 più vicina). Questo non influisce sulla correttezza del vostro algoritmo,
+   * vi dovete solo ricordare di paddare con un valore neutro per la vostra operazione. Nel caso del MAX il valore è -FLT_MAX oppure semplicemente FLT_MIN,
+   * invece se fate la somma e 0. La riduzione può essere usata per moltissime operazioni, bisogna solo cambiare il valore iniziale della variabile sum.
 	 */
     float sum = FLT_MIN;
     for (unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
